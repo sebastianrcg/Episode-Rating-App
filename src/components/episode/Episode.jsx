f9135c075239877be63005e99b15e2d2bdd21582
Sebastian Cruz <sebalox92@gmail.com>
2026-04-24 03:49:34 -0400

    begin comments

--- src/components/episode/Episode.jsx
+++ src/components/episode/Episode.jsx
@@ -7,29 +7,36 @@ import { CiStar } from "react-icons/ci";
 import { CloseButton, Dialog, Portal } from "@chakra-ui/react"
 import { useState } from "react";
 import { useRating } from "../../context/RatingProvider";
+import { Box, Collapsible, Stack } from "@chakra-ui/react"
+import { GoComment } from "react-icons/go";
 
-const Episode = ({ id,name, airDate, episode }) => {
-    const [open, setOpen] = useState(false);
+const Episode = ({ id, name, airDate, episode }) => {
+    const [openModal, setOpenModal] = useState(false);
     const [newRating, setNewRating] = useState(0);
-    const {ratings, addRating} = useRating();
+    const [openComment, setOpenComment] = useState(false);
 
 
-    const getAverage = (episodeId) =>{
-        if (!ratings[episodeId]){
+
+
+    const { ratings, addRating } = useRating();
+
+
+    const getAverage = (episodeId) => {
+        if (!ratings[episodeId]) {
             return 0
         } else {
             const episodeRatings = ratings[episodeId];
             const ratingsSum = episodeRatings.reduce((total, value) => total + value, 0);
-            const avg = (ratingsSum/(episodeRatings.length))
+            const avg = (ratingsSum / (episodeRatings.length))
             // round to nearest 0.5
-            return Math.round(avg * 2)/2 ;
+            return Math.round(avg * 2) / 2;
         }
     }
 
-    const onSubmitRating = (episodeId, rating) =>{
+    const onSubmitRating = (episodeId, rating) => {
         addRating(episodeId, rating);
         setNewRating(0);
-        setOpen(false);
+        setOpenModal(false);
     }
 
     return (
@@ -59,7 +66,7 @@ const Episode = ({ id,name, airDate, episode }) => {
 
                     {/* <Button variant={"outline"} padding="2" colorPalette={"blue"}><CiStar /> Rate</Button> */}
 
-                    <Dialog.Root lazyMount open={open} onOpenChange={(e) => setOpen(e.open)} placement="center">
+                    <Dialog.Root lazyMount open={openModal} onOpenChange={(e) => setOpenModal(e.open)} placement="center">
                         <Dialog.Trigger asChild>
                             {/* <Button variant="outline">Open</Button> */}
                             <Button variant={"outline"} padding="2" colorPalette={"blue"}><CiStar /> Rate</Button>
@@ -74,8 +81,8 @@ const Episode = ({ id,name, airDate, episode }) => {
                                     </Dialog.Header>
                                     <Dialog.Body display={"flex"} justifyContent="space-between" padding={"2"}>
 
-                                    {/* {Modal rating} */}
-                                        <RatingGroup.Root color="yellow" size="lg" count={5} value={newRating} onValueChange={(e)=> setNewRating(e.value)} allowHalf colorPalette={"yellow"}>
+                                        {/* {Modal rating} */}
+                                        <RatingGroup.Root color="yellow" size="lg" count={5} value={newRating} onValueChange={(e) => setNewRating(e.value)} allowHalf colorPalette={"yellow"}>
                                             <RatingGroup.Label />
                                             <RatingGroup.HiddenInput />
                                             <RatingGroup.Control>
@@ -85,16 +92,16 @@ const Episode = ({ id,name, airDate, episode }) => {
                                             </RatingGroup.Control>
                                         </RatingGroup.Root>
 
-                                    
-                                    <Dialog.Footer>
-                                        <Dialog.ActionTrigger asChild>
-                                            <Button  padding={"2"}variant="outline">Cancel</Button>
-                                        </Dialog.ActionTrigger>
-                                        <Button onClick={()=> onSubmitRating(id, newRating)} variant={"outline"} padding="2" colorPalette={"blue"}>Rate</Button>
-                                    </Dialog.Footer>
+
+                                        <Dialog.Footer>
+                                            <Dialog.ActionTrigger asChild>
+                                                <Button padding={"2"} variant="outline">Cancel</Button>
+                                            </Dialog.ActionTrigger>
+                                            <Button onClick={() => onSubmitRating(id, newRating)} variant={"outline"} padding="2" colorPalette={"blue"}>Rate</Button>
+                                        </Dialog.Footer>
 
                                     </Dialog.Body>
-                                    
+
                                     <Dialog.CloseTrigger asChild>
                                         <CloseButton size="sm" />
                                     </Dialog.CloseTrigger>
@@ -102,7 +109,25 @@ const Episode = ({ id,name, airDate, episode }) => {
                             </Dialog.Positioner>
                         </Portal>
                     </Dialog.Root>
+
+                    {/* {end of modal} */}
+
+                    <Separator orientation="vertical" height="6" />
+                    <Button onClick={()=> setOpenComment(!openComment)} variant={"outline"} padding="2" colorPalette={"blue"}><GoComment /> Comment</Button>
+
+
                 </Card.Footer>
+                <Collapsible.Root marginTop={"10px"} open={openComment} onOpenChange={(e) => setOpenComment(e.open)}>
+                    
+
+                    <Collapsible.Content>
+                        {/* <Box padding="4" borderWidth="1px">
+                            This collapsible is controlled by external state. You can open and
+                            close it using the buttons above or by clicking the trigger.
+                        </Box> */}
+                        <textarea className={classes.comment}></textarea>
+                    </Collapsible.Content>
+                </Collapsible.Root>
             </Card.Root>
 
 
